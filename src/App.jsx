2762cd367d987e024852e5bf910481789e0fb7e7
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-05-03 00:23:07 +0000

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      <Cv />
+      <GeneralInfo />
+      <Education />
+      <Career />
+    </main>
+  )
+}
+
+
+function Cv(){
+  return (
+    <div>
+      <p>Test</p>
+    </div>
+  )
+}
+
+function GeneralInfo(){
+  return (
+    <div>
+      <p>general</p>
+    </div>
+  )
+}
+
+function Education(){
+  return (
+    <div>
+      <p>education</p>
+    </div>
+  )
+}
+
+function Career(){
+  return 
+    <div>
+      <p>career</p>
+    </div>
+}
